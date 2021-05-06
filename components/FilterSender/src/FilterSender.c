4f4580d17cc3fdaf5066f840a83d3d2fd5047129
Matthias Buhl <matthias.buhl@hensoldt-cyber.de>
2021-05-06 14:09:13 +0200

    [DEV] SEOS-2659: Connect FilterSender and FilterListener via RPC

--- components/FilterSender/src/FilterSender.c
+++ components/FilterSender/src/FilterSender.c
@@ -6,24 +6,44 @@
 
 #include "system_config.h"
 
-#include "lib_compiler/compiler.h"
 #include "lib_debug/Debug.h"
 #include <camkes.h>
-#include <string.h>
 
 #include "OS_Error.h"
 #include "OS_Network.h"
 #include "OS_NetworkStackClient.h"
 
 //------------------------------------------------------------------------------
+typedef struct
+{
+    OS_Dataport_t       dataport;
+    OS_Network_Socket_t tcpSocket;
+}
+FilterSender_t;
+
+static FilterSender_t ctx =
+{
+    .dataport   = OS_DATAPORT_ASSIGN(filterSender_port),
+    .tcpSocket  =
+    {
+        .domain = OS_AF_INET,
+        .type   = OS_SOCK_STREAM,
+        .name   = FILTER_SENDER_IP_ADDR,
+        .port   = FILTER_SENDER_PORT
+    },
+};
+
+//------------------------------------------------------------------------------
 static void
 init_network_client_api(void)
 {
-    static OS_Dataport_t dataports[FILTER_SENDER_NUM_SOCKETS] = {
+    static OS_Dataport_t dataports[FILTER_SENDER_NUM_SOCKETS] =
+    {
         OS_DATAPORT_ASSIGN(socket_1_port)
     };
 
-    static OS_NetworkStackClient_SocketDataports_t config = {
+    static OS_NetworkStackClient_SocketDataports_t config =
+    {
         .number_of_sockets = ARRAY_SIZE(dataports),
         .dataport = dataports
     };
@@ -32,79 +52,70 @@ init_network_client_api(void)
 }
 
 //------------------------------------------------------------------------------
-int
-run(void)
+OS_Error_t
+filterSender_rpc_forwardRecvData(
+    const size_t requestedLen,
+    size_t* const actualLen)
 {
-    // TODO: All of the following code is meant as a simple test and will be
-    // replaced with the actual demo application code once we get to that step
-    // with SEOS-2659. The idea for now is to send a few test messages that can
-    // be received by the Receiver Python App running on the host.
-    Debug_LOG_INFO("Starting Filter Client...");
-
-    init_network_client_api();
-
-    OS_Network_Socket_t tcpSocket =
-    {
-        .domain = OS_AF_INET,
-        .type   = OS_SOCK_STREAM,
-        .name   = FILTER_SENDER_IP_ADDR,
-        .port   = FILTER_SENDER_PORT
-    };
-
     OS_NetworkSocket_Handle_t hClient;
     OS_Error_t ret = OS_NetworkSocket_create(
                          NULL,
-                         &tcpSocket,
+                         &ctx.tcpSocket,
                          &hClient);
     if (ret != OS_SUCCESS)
     {
-        Debug_LOG_ERROR("OS_NetworkServerSocket_create() failed, code %d", ret);
-        return -1;
+        Debug_LOG_ERROR("OS_NetworkSocket_create() failed, code %d", ret);
+        return ret;
     }
 
-    // Send a few test messages to verify the system setup is functional.
-    for (unsigned int i = 0; i < 10; i++)
+    const void* offset = OS_Dataport_getBuf(ctx.dataport);
+    const size_t dpSize = OS_Dataport_getSize(ctx.dataport);
+
+    if (requestedLen > dpSize)
     {
-        char request[26];
+        Debug_LOG_ERROR("Requested length %zu exceeds dataport size %zu",
+                        requestedLen, dpSize);
+        return OS_ERROR_INVALID_PARAMETER;
+    }
 
-        DECL_UNUSED_VAR(int lenNeeded) = snprintf(
-                                             request,
-                                             sizeof(request),
-                                             "Client Hello Message #%u\n",
-                                             (i + 1));
+    size_t sumLenWritten = 0;
 
-        Debug_ASSERT((lenNeeded > 0) && (lenNeeded < sizeof(request)));
+    while (sumLenWritten < requestedLen)
+    {
+        size_t lenWritten = 0;
 
-        const size_t lenRequest = strlen(request);
-        size_t sumLenWritten = 0;
+        ret = OS_NetworkSocket_write(
+                  hClient,
+                  offset,
+                  requestedLen - sumLenWritten,
+                  &lenWritten);
 
-        while (sumLenWritten < lenRequest)
+        sumLenWritten += lenWritten;
+
+        if (ret != OS_SUCCESS)
         {
-            size_t actualLenWritten = 0;
-
-            ret = OS_NetworkSocket_write(
-                      hClient,
-                      &request[sumLenWritten],
-                      lenRequest - sumLenWritten,
-                      &actualLenWritten);
-
-            if (ret != OS_SUCCESS)
-            {
-                Debug_LOG_ERROR(
-                    "OS_NetworkSocket_write() failed, code %d", ret);
-                OS_NetworkSocket_close(hClient);
-                return -1;
-            }
-
-            sumLenWritten += actualLenWritten;
+            Debug_LOG_ERROR(
+                "OS_NetworkSocket_write() failed, code %d", ret);
+            OS_NetworkSocket_close(hClient);
+            *actualLen = sumLenWritten;
+            return ret;
         }
 
-        Debug_LOG_INFO("Test message %u successfully sent", (i + 1));
+        offset += sumLenWritten;
     }
-
-    Debug_LOG_INFO("All test messages successfully sent");
 
     OS_NetworkSocket_close(hClient);
 
-    return 0;
+    *actualLen = sumLenWritten;
+
+    return OS_SUCCESS;
+}
+
+//------------------------------------------------------------------------------
+void
+post_init(void)
+{
+    Debug_LOG_INFO("Initializing Filter Sender");
+
+    init_network_client_api();
 }
